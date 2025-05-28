75baf6ecec652869cb64ed2fd733109deefb81e6
Adam-Elmi <Adamcade123@gmail.com>
2025-05-28 17:06:42 +0300

    Update config.lua

---
+++ core/config.lua
@@ -0,0 +1,17 @@
+local config = {
+  optimize_with_svgo = true,
+  default_output_type = "jsx",
+  default_output_location = "./generated",
+  use_specified_location = true,
+  specified_location = "./src/icons",
+}
+
+function config.set(key, value)
+  config[key] = value
+end
+
+function config.get(key)
+  return config[key]
+end
+
+return config
