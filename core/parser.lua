df98ce45ee3721df528a895b0fe32deeeca2af37
Adam-Elmi <Adamcade123@gmail.com>
2025-05-30 19:41:09 +0300

    Updated parser.lua

--- core/parser.lua
+++ core/parser.lua
@@ -12,14 +12,38 @@ function Parser:new()
     return obj
 end
 
-function Parser:singleParse(targetFile, outputPath)
+function Parser:singleParse(targetFile, outputPath, outputFile)
     if utils.exists(targetFile) then
         if (outputPath and utils.exists(outputPath)) or (config.default_output_location and utils.exists(config.default_output_location)) then
             local file = utils.getFile(targetFile, "svg")
             if config.default_output_type and config.default_output_type ~= "" then
-                if file then
-                    return utils.handleRawSvg(file.content, attributes.kebab_attributes,
-                        attributes.special_attributes)
+                if file and outputFile then
+                    local template_file = utils.getContent("./templates/react.tpl")
+                    print(template_file)
+                    local component_file = io.open(
+                        string.format("%s/%s.%s", config.default_output_location, outputFile, config.default_output_type),
+                        "w")
+                    if component_file and template_file then
+                        if config.import_as_default then
+                            component_file:write(string.format(template_file, "default", "ComponentIcon", "",
+                                utils.handleRawSvg(
+                                    file.content,
+                                    attributes.kebab_attributes,
+                                    attributes.special_attributes
+                                )))
+                        else
+                            component_file:write(string.format(template_file, "", "ComponentIcon", "",
+                                utils.handleRawSvg(
+                                    file.content,
+                                    attributes.kebab_attributes,
+                                    attributes.special_attributes
+                                )))
+                        end
+                        component_file:close()
+                    else
+                        print(select(2,
+                            utils.customError("File Error", "Failed to open file for writing: " .. outputFile)))
+                    end
                 end
             end
         else
