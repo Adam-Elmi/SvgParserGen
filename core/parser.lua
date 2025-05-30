3e6be66f1bfe6b1a4a14c7cd1e8c5adefc20045e
Adam-Elmi <Adamcade123@gmail.com>
2025-05-30 09:46:04 +0300

    Updated parser.lua

--- core/parser.lua
+++ core/parser.lua
@@ -1,3 +1,6 @@
+local config = require("config")
+local utils = require("core.utils")
+
 local Parser = {}
 Parser.__index = Parser
 setmetatable(Parser, {})
@@ -8,3 +11,20 @@ function Parser:new()
     return obj
 end
 
+function Parser:singleParse(targetFile, outputPath)
+    if targetFile then
+        if outputPath and outputPath ~= "" or config.default_output_location and config.default_output_location ~= "" then
+            return true
+        else
+            local _, error = utils.customError(utils.colorize("Path Error:", "red"),
+                utils.colorize("Output path is not provided! Please provide the output path.", "magenta"))
+                return error
+        end
+    else
+        local _, error = utils.customError(utils.colorize("Path Error:", "red"),
+            utils.colorize("Target path is not provided! Please provide the target path.", "magenta"))
+            return error
+    end
+end
+
+print(Parser:singleParse("Hello"))
