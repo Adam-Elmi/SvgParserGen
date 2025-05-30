e0a47d08bfb9d216130e44d28bb011f5a15a47fa
Adam-Elmi <Adamcade123@gmail.com>
2025-05-30 12:29:19 +0300

    Updated parser.lua

--- core/parser.lua
+++ core/parser.lua
@@ -1,5 +1,6 @@
 local config = require("config")
 local utils = require("core.utils")
+local attributes = require("core.attributes")
 
 local Parser = {}
 Parser.__index = Parser
@@ -14,17 +15,22 @@ end
 function Parser:singleParse(targetFile, outputPath)
     if targetFile then
         if outputPath and outputPath ~= "" or config.default_output_location and config.default_output_location ~= "" then
-            return true
+            local file = utils.getFile(targetFile, "svg")
+            if config.default_output_type and config.default_output_type ~= "" then
+                if file then
+                    print(file)
+                    return utils.handleAttributes(file.content, attributes.kebab_attributes,
+                        attributes.special_attributes)
+                end
+            end
         else
-            local _, error = utils.customError(utils.colorize("Path Error", "red"),
-                utils.colorize("Output path is not provided! Please provide the output path.", "magenta"))
-                return error
+            return select(2, utils.customError(utils.colorize("Path Error", "red"),
+                utils.colorize("Output path is not provided! Please provide the output path.", "magenta")))
         end
     else
-        local _, error = utils.customError(utils.colorize("Path Error", "red"),
-            utils.colorize("Target path is not provided! Please provide the target path.", "magenta"))
-            return error
+        return select(2, utils.customError(utils.colorize("Path Error", "red"),
+            utils.colorize("Target path is not provided! Please provide the target path.", "magenta")))
     end
 end
 
-print(Parser:singleParse("Hello"))
+print(Parser:singleParse("./core/example/test.svg"))
