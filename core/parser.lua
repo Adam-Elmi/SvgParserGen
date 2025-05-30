1bd263092416c0720fadb2489aa925510d61a758
Adam-Elmi <Adamcade123@gmail.com>
2025-05-30 13:11:30 +0300

    Updated parser.lua

--- core/parser.lua
+++ core/parser.lua
@@ -13,23 +13,24 @@ function Parser:new()
 end
 
 function Parser:singleParse(targetFile, outputPath)
-    if targetFile then
-        if outputPath and outputPath ~= "" or config.default_output_location and config.default_output_location ~= "" then
+    if utils.exists(targetFile) then
+        if (outputPath and utils.exists(outputPath)) or (config.default_output_location and utils.exists(config.default_output_location)) then
             local file = utils.getFile(targetFile, "svg")
             if config.default_output_type and config.default_output_type ~= "" then
                 if file then
-                    print(file)
-                    return utils.handleAttributes(file.content, attributes.kebab_attributes,
+                    return utils.handleRawSvg(file.content, attributes.kebab_attributes,
                         attributes.special_attributes)
                 end
             end
         else
-            return select(2, utils.customError(utils.colorize("Path Error", "red"),
-                utils.colorize("Output path is not provided! Please provide the output path.", "magenta")))
+            return select(2,
+                utils.customError("Path Error",
+                    string.format("%s is invalid path! Please provide a valid output path.",
+                        (outputPath or config.default_output_location))))
         end
     else
-        return select(2, utils.customError(utils.colorize("Path Error", "red"),
-            utils.colorize("Target path is not provided! Please provide the target path.", "magenta")))
+        return select(2,
+            utils.customError("Path Error", "Target path is invalid path! Please provide a valid target path."))
     end
 end
 
