c6aa25d6ae94320a2c99b15b0acb2310a2e7d0a4
Adam-Elmi <Adamcade123@gmail.com>
2025-05-30 09:49:43 +0300

    Updated Something :(

--- core/parser.lua
+++ core/parser.lua
@@ -16,12 +16,12 @@ function Parser:singleParse(targetFile, outputPath)
         if outputPath and outputPath ~= "" or config.default_output_location and config.default_output_location ~= "" then
             return true
         else
-            local _, error = utils.customError(utils.colorize("Path Error:", "red"),
+            local _, error = utils.customError(utils.colorize("Path Error", "red"),
                 utils.colorize("Output path is not provided! Please provide the output path.", "magenta"))
                 return error
         end
     else
-        local _, error = utils.customError(utils.colorize("Path Error:", "red"),
+        local _, error = utils.customError(utils.colorize("Path Error", "red"),
             utils.colorize("Target path is not provided! Please provide the target path.", "magenta"))
             return error
     end
