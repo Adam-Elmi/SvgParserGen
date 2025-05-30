ba4f11a4a2d1fbd76eb9c93880ed3fc69edb490d
Adam-Elmi <Adamcade123@gmail.com>
2025-05-30 12:29:11 +0300

    Updated config.lua

--- config.lua
+++ config.lua
@@ -3,10 +3,16 @@ local allow_typescript = false
 local config = {
   optimize_with_svgo = true,
   default_output_type = allow_typescript and "tsx" or "jsx",
-  default_output_location = "",
+  default_output_location = "./generated",
   use_specified_location = true,
   specified_location = "./src/icons",
   use_windows_format_path = true,
+  accepted_extensions = {
+    svg = true,
+    jsx = true,
+    tsx = true,
+    astro = true
+  },
   prettify = false
 }
 
