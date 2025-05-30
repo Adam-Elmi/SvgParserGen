2dfb3dd09084023df6b22c7d32689f6d48c160b4
Adam-Elmi <Adamcade123@gmail.com>
2025-05-30 17:20:16 +0300

    Added project.lua

---
+++ project.lua
@@ -0,0 +1,15 @@
+return {
+    name = "SvgParserGen",
+    description = "A command-line tool for optimizing SVG files and generating SVG component code.",
+    version = "0.1.0",
+    author = "Adam Elmi",
+    license = "MIT",
+    github = {
+        username = "Adam-Elmi",
+        repo = "https://github.com/Adam-Elmi/SvgParserGen",
+        homepage = "https://github.com/Adam-Elmi/SvgParserGen#readme",
+        readme = "https://github.com/Adam-Elmi/SvgParserGen#readme",
+        issue = "https://github.com/Adam-Elmi/SvgParserGen/issues",
+    },
+    featurcos = {}
+}
