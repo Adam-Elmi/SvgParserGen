fa48cb7497483c4fd13171ae5a7a5176ba39b937
Adam-Elmi <Adamcade123@gmail.com>
2025-05-31 23:33:56 +0300

    Update parser.lua

--- core/parser.lua
+++ core/parser.lua
@@ -12,6 +12,72 @@ function Parser:new()
     return obj
 end
 
+
+local function handleAttr(content, attr, sep)
+    if content and attr then
+        for _, v in ipairs(attr) do
+            if v then
+                if sep == "-" then
+                    local camelCaseValue = utils.toCamelCase(v, sep)
+                    local safeValue = utils.escape_pattern(v)
+                    content = string.gsub(content, safeValue, camelCaseValue)
+                elseif sep == "special" then
+                    local specialValue = ""
+                    if v == "class" then
+                        specialValue = "className"
+                    elseif v == "xlink:href" then
+                        specialValue = "xlinkHref"
+                    else
+                        return select(2, utils.customError("Special Error", "Unrecognized special attribute"))
+                    end
+                    local safeValue = utils.escape_pattern(v)
+                    content = string.gsub(content, safeValue, specialValue)
+                else
+                    return select(2, utils.customError("Error", "Separator is undefined or invalid"))
+                end
+            else
+                return select(2, utils.customError("Content Error", "Attribute is nil or false"))
+            end
+        end
+    end
+    return content
+end
+
+local function handleRawSvg(content, kebab_attributes, special_attributes)
+    if content then
+        if kebab_attributes and #kebab_attributes > 0 then
+            content = handleAttr(content, kebab_attributes, "-")
+        end
+        if special_attributes and #special_attributes > 0 then
+            content = handleAttr(content, special_attributes, "special")
+        end
+        return content
+    end
+end
+
+local function handleJSX(content)
+    local kebab_attri = {}
+    for _, v in ipairs(attributes.kebab_attributes) do
+        kebab_attri[v] = utils.toCamelCase(v, "-")
+    end
+    if content then
+        local svg_content = string.match(content, "<svg.-</svg>")
+        if svg_content then
+            for k, v in pairs(kebab_attri) do
+                svg_content = string.gsub(svg_content, v, k)
+            end
+            for _, v in ipairs(attributes.special_attributes) do
+                if v == "class" then
+                    svg_content = string.gsub(svg_content, "className", v)
+                elseif v == "xlink:href" then
+                    svg_content = string.gsub(svg_content, "xlinkHref", v)
+                end
+            end
+            return svg_content
+        end
+    end
+end
+
 local function missingFile(file, outputFile)
     local missing = {}
     if not file then table.insert(missing, "file") end
@@ -55,7 +121,7 @@ function Parser:toJSX(targetFile, outputPath, outputFile, componentName)
                                 if config.import_as_default then
                                     component_file:write(string.format(template_file, "default",
                                         (componentName and componentName ~= "" and componentName or compName), "",
-                                        utils.handleRawSvg(
+                                        handleRawSvg(
                                             file.content,
                                             attributes.kebab_attributes,
                                             attributes.special_attributes
@@ -63,7 +129,7 @@ function Parser:toJSX(targetFile, outputPath, outputFile, componentName)
                                 else
                                     component_file:write(string.format(template_file, "",
                                         (componentName and componentName ~= "" and componentName or compName), "",
-                                        utils.handleRawSvg(
+                                        handleRawSvg(
                                             file.content,
                                             attributes.kebab_attributes,
                                             attributes.special_attributes
@@ -117,7 +183,7 @@ function Parser:toSVG(targetFile, outputPath, outputFile)
                 if file.content and file.content ~= "" then
                     local newFile = io.open(string.format("%s/%s.%s", outputPath, outputFile, "svg"), "w")
                     if newFile then
-                        newFile:write(utils.handleJSX(file.content))
+                        newFile:write(handleJSX(file.content))
                     end
                 else
                     emptyContent(targetFile)
