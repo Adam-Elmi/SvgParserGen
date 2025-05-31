0f03ce2e0102eafb98cd20beb45e2964406c51e5
Adam-Elmi <Adamcade123@gmail.com>
2025-05-31 09:06:58 +0300

    Fixed a minor error

--- core/parser.lua
+++ core/parser.lua
@@ -12,16 +12,15 @@ function Parser:new()
     return obj
 end
 
-function Parser:singleParse(targetFile, outputPath, outputFile)
+function Parser:toJSX(targetFile, outputPath, outputFile)
     if utils.exists(targetFile) then
         if (outputPath and utils.exists(outputPath)) or (config.default_output_location and utils.exists(config.default_output_location)) then
             local file = utils.getFile(targetFile, "svg")
             if config.default_output_type and config.default_output_type ~= "" then
                 if file and outputFile then
                     local template_file = utils.getContent("./templates/react.tpl")
-                    print(template_file)
                     local component_file = io.open(
-                        string.format("%s/%s.%s", config.default_output_location, outputFile, config.default_output_type),
+                        string.format("%s/%s.%s", outputPath, outputFile, config.default_output_type),
                         "w")
                     if component_file and template_file then
                         if config.import_as_default then
@@ -41,8 +40,10 @@ function Parser:singleParse(targetFile, outputPath, outputFile)
                         end
                         component_file:close()
                     else
-                        print(select(2,
-                            utils.customError("File Error", "Failed to open file for writing: " .. outputFile)))
+                       if outputFile then
+                           print(select(2,
+                               utils.customError("File Error", "Failed to open file for writing: " .. outputFile)))
+                       end
                     end
                 end
             end
