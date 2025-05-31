2826e3191b3ab85bf6bd3ddda2bc7836b64ea300
Adam-Elmi <Adamcade123@gmail.com>
2025-05-31 14:08:30 +0300

    Added default_component_name and use_same_name

--- config.lua
+++ config.lua
@@ -3,11 +3,12 @@ local allow_typescript = false
 local config = {
   optimize_with_svgo = true,
   default_output_type = allow_typescript and "tsx" or "jsx",
-  default_output_location = "./generated",
+  default_output_location = "",
   use_specified_location = true,
   specified_location = "./src/icons",
   import_as_default = false,
   default_component_name = "",
+  use_same_name = true,
   template_attributes_to_include = {width = true, height = true},
   use_windows_format_path = true,
   accepted_extensions = {
